f7443c5f6a26ca38e10bd101f367d2e714282e6e
timofey <timofey.gudnev@nami.ru>
2021-07-29 10:43:26 +0300

    map_inverse added

--- topic_explorer/include/topic_explorer/topic_explorer.h
+++ topic_explorer/include/topic_explorer/topic_explorer.h
@@ -8,23 +8,22 @@
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
 
 class Texplorer : public rclcpp::Node
 {
   public:
     Texplorer();
 
-
   private:
 
     void print_map(const std::map<std::string, std::vector<std::string>>& m);
+    void print_map(const std::map<std::vector<std::string>, std::string>& m);
     rclcpp::SubscriptionBase::SharedPtr universal_sub_;
     void Init();
     void timer_callback();
     rclcpp::TimerBase::SharedPtr timer_;
     //rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    std::map<std::string, std::vector<std::string>> topiclist_;
     size_t count_;
 };
 
--- topic_explorer/src/topic_explorer.cpp
+++ topic_explorer/src/topic_explorer.cpp
@@ -9,20 +9,33 @@
     }
 
     void Texplorer::Init(){
-          std::map<std::string, std::vector<std::string>> topiclist;
 
-          topiclist = get_topic_names_and_types();
-          int topicnum = topiclist.size();
-          print_map(topiclist);
+          topiclist_ = get_topic_names_and_types();
+
+            std::map<std::vector<std::string>, std::string> inverse_map;
+            for (std::map<std::string, std::vector<std::string>>::iterator it = topiclist_.begin(); it != topiclist_.end(); ++it) {
+                inverse_map[it->second]=it->first;
+            }
+          int topicnum = topiclist_.size();
+          print_map(topiclist_);
+          print_map(inverse_map);
         // universal_sub_ = ;
     }
 
-    void Texplorer::timer_callback(){}
+    void Texplorer::timer_callback(){
+
+        std::string test = "abc";
+    }
 
     void Texplorer::print_map(const std::map<std::string, std::vector<std::string>>& m){
         for (const auto& [key, value] : m) {
         std::cout << key << " = " << value.at(0) << "; " << std::endl;
+        }
     }
 
-}
+    void Texplorer::print_map(const std::map<std::vector<std::string>, std::string>& m){
+        for (const auto& [key, value] : m) {
+        std::cout << key.at(0) << " = " << value << "; " << std::endl;
+        }
+    }
  
